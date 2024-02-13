bae895696858627331656916e9bcbb0886706d9a
Rahul Kumar <me.rahulkumar@icloud.com>
2024-02-13 12:16:06 +0530

    fix: linting activate event

--- nvim/lua/plugins/linting.lua
+++ nvim/lua/plugins/linting.lua
@@ -17,7 +17,7 @@ return {
 		-- linting in normal mode
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "DiagnosticChanged" }, {
 			group = lint_augroup,
 			callback = function()
 				lint.try_lint()

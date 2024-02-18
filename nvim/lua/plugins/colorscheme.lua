90ae1c0e63225bb7b19c26ed90bb05391413d20d
Rahul Kumar <me.rahulkumar@icloud.com>
2024-02-18 15:20:10 +0530

    added gruvbox colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,12 @@
 return {
 	{
-		"rose-pine/neovim",
-		as = "rose-pine",
-		lazy = false,
+		"ellisonleao/gruvbox.nvim",
 		config = function()
-			require("rose-pine").setup({
-				variant = "main",
-				disable_background = true,
+			require("gruvbox").setup({
+				transparent_mode = true,
 			})
-			vim.cmd("colorscheme rose-pine")
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+			vim.o.background = "dark" -- or "light" for light mode
+			vim.cmd("colorscheme gruvbox")
 		end,
 	},
 }

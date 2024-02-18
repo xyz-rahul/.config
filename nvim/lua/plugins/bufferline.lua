514a238aee03794d4277dff9de86242025101f13
Rahul Kumar <me.rahulkumar@icloud.com>
2024-02-18 15:35:37 +0530

    need to rethink

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -4,13 +4,15 @@ return {
 	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
 		vim.opt.termguicolors = true
-		require("bufferline").setup({
-
-			options = {
-				numbers = function(opts)
-					return string.format("%s", opts.id, opts.id)
-				end,
-			},
-		})
+		-- require("bufferline").setup({
+		--
+		-- 	options = {
+		-- 		mode = "buffers", -- set to "tabs" to only show tabpages instead
+		--
+		-- 		indicator = {
+		-- 			style = "underline",
+		-- 		},
+		-- 	},
+		-- })
 	end,
 }

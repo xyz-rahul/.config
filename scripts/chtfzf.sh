33e412c2334bd96d14bacab9992fd8717317dc69
Rahul Kumar <me.rahulkumar@icloud.com>
2024-03-12 21:21:11 +0530

    added cht.sh and updated files

--- scripts/chtfzf.sh
+++ scripts/chtfzf.sh
@@ -1,138 +1,39 @@
 #!/usr/bin/env bash
-# chtfzf.sh
-# by iotku
-# license: wtfpl
-# Requirements: curl, fzf
 
-# Docs: use -t flag to launch in a new tmux window
-#   - ``chtfzf sync`` to cache main list for faster access
+CHT_SH_LIST_CACHE_DIR=$HOME/'.cache/'
+CHT_SH_LIST_CACHE=$HOME/'.cache/cht_sh_cached_list'
 
-set -euf -o pipefail
-CACHE_DIR="${HOME}/.local/share/chtfzf"
-openMode="bash"
-
-function main {
-    # Read Command Line Arguments
-    for i in "$@"; do
-        case "$i" in
-            -t) openMode="tmux"; shift;;
-            sync) syncSheetList;;
-            query) shift; query $*;;
-            preview) shift; cachePreview "$*";;
-            *) ;; # Do nothing if no matches
-        esac
-    done
-
-    search="$(searchMain)"
-    pushd -n "$search" > /dev/null 2>&1
-    if [[ "${search: -1}" != "/" ]]; then
-        openSheet "$search"
-        exit
+#Cache the list on first run
+if [ ! -f "$CHT_SH_LIST_CACHE" ]; then
+    if [ ! -d "$CHT_SH_LIST_CACHE_DIR" ]; then
+        mkdir $CHT_SH_LIST_CACHE_DIR
     fi
+    echo "First time run. Downloading cht.sh cht.sh/:list to cache..."
+    curl cht.sh/:list > $CHT_SH_LIST_CACHE
+fi
 
-    while :; do # do while
-        search=$(curl -sg "cht.sh/$(getPath):list" | 
-            grep -v ":list" | 
-            fzf --bind "ctrl-d:print-query" --preview="${BASH_SOURCE[0]} preview "$(getPath){}"")
 
-        if [[ "$search" == "" ]]; then # go up a level on ctrl-d
-            popd -n > /dev/null 2>&1
-            if [[ $? != 0 ]]; then
-                break
-            fi
-            continue
-        fi
-        [[ "${search: -1}" == "/" ]] || break # exit condition
-        pushd -n "$search" > /dev/null 2>&1
-    done
-    
-    if [[ "$search" == "" ]]; then
-        main
-        exit
-    fi
-    echo "opening $(getPath)$search"
-    openSheet "$(getPath)$search"
-}
+#Select a cht.sh cheat from the list
+selected=`cat $CHT_SH_LIST_CACHE | fzf --reverse --height 75% --border -m --ansi --nth 2..,.. --prompt='CHEAT.SH> ' --preview='curl -s cht.sh/{}' --preview-window=right:80%`
+if [[ -z $selected ]]; then
+    exit 0
+fi
 
-function getPath {
-    echo "$(dirs -p | tail -n +2 | sed 's/.$//' | tac | tr '\n' '/')"
-}
+# Ask the user what they would like to query (with live preview, also displays preview for topics not given by cheat.sh/$selected/:list)
+query=`curl -s cht.sh/$selected/:list | fzf --print-query --reverse --height 75% --border -m --ansi --nth 2..,.. --prompt='CHEAT.SH/'$(echo $selected | tr a-z A-Z)'> ' \
+    --preview='curl -s cht.sh/'$selected'/{1} && curl -s cht.sh/'$selected'/{q}' --preview-window=right:80%`
+# BUG: Fix below above logic so it displays preview when there is an available match to query, otherwise attempts to curl unmatched query
 
-function openSheet {
-    if [[ "$1" == "" ]]; then exit; fi # Exit if empty string
-    case "$openMode" in
-        tmux) tmux neww bash -c "curl -sg "cht.sh/$*" | less -R";;
-        bash) curl -sg "cht.sh/$*" | less -R;;
-        *) echo "Unknown openMode, set -t to use tmux, or no args to use bash directly"
-    esac
-}
 
-function searchMain {
-    if [ -f "$CACHE_DIR/main.list" ]; then
-        # Use cached list if it exists
-        echo "$(grep -v ":list" "$CACHE_DIR/main.list" | fzf --query="$*" --preview="${BASH_SOURCE[0]} preview '{}'")"
+#Retreive the cheatsheet from cht.sh
+if grep -qs "$selected" $CHT_SH_LIST_CACHE; then
+    query=`echo $query | tr ' ' '+' | sed 's/.*+//'`
+    if [[ -z $query ]]; then
+        echo "curl cht.sh/$selected" & curl cht.sh/$selected
     else
-        echo "$(curl -sg "cht.sh/:list" | grep -v ":list" | fzf --query="$*" --preview="${BASH_SOURCE[0]} preview '{}'")"
+        echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query
     fi
-}
-
-function query {
-    search="$(searchMain)"
-    read -rp "query: " queryInput
-    queryInput=$(tr ' ' '+' <<< "$queryInput")
-    if [[ "$queryInput" == "" ]]; then
-        openSheet "$(echo "$search")"
-        exit
-    fi
-
-    if [[ "${search: -1}" != "/" ]]; then
-        openSheet "$search/$queryInput"
-    else
-        openSheet "$search$queryInput"
-    fi
-    
-    exit
-}
-
-function syncSheetList {
-    [ ! -d "$CACHE_DIR" ] && mkdir -p "$CACHE_DIR"
-    printf "Saving main.list to %s\nDon't forget to sync in the future :)\n" "$CACHE_DIR"
-    curl "cht.sh/:list" > "$CACHE_DIR/main.list"
-    printf "done.\n"
-    exit
-}
-
-function cachePreview {
-    # Hopefully avoid really bad situations
-    if [[ "$*" == "" ]]; then exit; fi
-    if [[ "$*" == "/" ]]; then exit; fi
-
-    # Just use curl directly if we don't have a cache directory (haven't done sync yet)
-    if [ ! -d "$CACHE_DIR" ]; then
-       curl -sg "cht.sh/$*"
-       exit
-    fi
-
-    # Exists in cache
-    if [ -f "$CACHE_DIR/$*.sheet" ]; then
-        cat "$CACHE_DIR/$*.sheet"
-        exit
-    fi
-
-    # Make subdirectory structure to match preview
-    SUB_DIR="$(dirname "$CACHE_DIR/$*.sheet")"
-    [ ! -d  "$SUB_DIR" ] && mkdir -p "$SUB_DIR"
-    # sheet with status code as last line
-    sheet="$(curl -sg -w "%{http_code}" "cht.sh/$*")"
-    statusCode="$(tail -n1 <<< "$sheet")"
-    sheet="$(sed '$d' <<< "$sheet")" # Remove status code from output
-    if [[ "$statusCode" == "200" ]]; then
-        tee "$CACHE_DIR/$*.sheet" <<< "$sheet"
-    else
-        # Don't cache error page
-        printf "Sorry, cht.sh returned non 200 status code (error):\n%s" "$sheet"
-    fi
-    exit
-}
-
-main $@ # Pass CLI args to main 
+else
+    #If its not in the cache, query cht.sh anyways and hope for the best
+    curl -s cht.sh/$selected~$query
+fi

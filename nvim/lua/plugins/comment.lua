9ad17e7ab09d4e48b6c37235ae14ec677e731903
Rahul Kumar <me.rahulkumar@icloud.com>
2024-03-21 09:39:30 +0530

    updated comment: when file contains diff languages e.g. jsx

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,7 +1,22 @@
 return {
-	"numToStr/Comment.nvim",
-	opts = {
-		-- add any options here
+	{
+		"numToStr/Comment.nvim",
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+		},
+		lazy = false,
+		config = function()
+			require("Comment").setup({
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			})
+		end,
 	},
-	lazy = false,
+
+	{
+		"folke/todo-comments.nvim",
+		event = "BufRead",
+		config = function()
+			require("todo-comments").setup()
+		end,
+	},
 }
